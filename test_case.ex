c53167b2fcbb2f7298a1bac1a100dd7aeab4a61d
Warren Sadler <warrensadler@CORPLYI7725MBP.local>
2018-07-15 22:06:50 -0500

    add failing assertion in test case

--- test_case.ex
+++ test_case.ex
@@ -5,8 +5,10 @@ defmodule Example do
   def run do
     Hound.start_session()
 
-    navigate_to("http://google.com")
-    assert "Goggle" == page_title()
+    test "the title is correct" do
+      navigate_to("http://google.com")
+      assert "Goggle" == page_title()
+    end
 
     Hound.end_session()
   end
