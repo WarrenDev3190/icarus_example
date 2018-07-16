aaca3c290084583d48c9526ddf02d0ce8a753570
Warren Sadler <warrensadler@CORPLYI7725MBP.local>
2018-07-15 22:09:08 -0500

    remove from def

--- test_case.ex
+++ test_case.ex
@@ -2,14 +2,10 @@ defmodule Example do
   use ExUnit.Case
   use Hound.Helpers
 
-  def run do
-    Hound.start_session()
+  hound_session()
 
-    test "the title is correct" do
-      navigate_to("http://google.com")
-      assert "Goggle" == page_title()
-    end
-
-    Hound.end_session()
+  test "the title is correct" do
+    navigate_to("http://google.com")
+    assert "Goggle" == page_title()
   end
 end
